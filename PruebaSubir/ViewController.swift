f98a30e2331efc75a6a01c500f309026f247a6d4
James Montoya <user@MacBook-Pro-de-user.local>
2016-01-06 21:02:27 -0500

    Initial Commit

---
+++ PruebaSubir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaSubir
+//
+//  Created by James Montoya on 6/01/16.
+//  Copyright (c) 2016 James Montoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
